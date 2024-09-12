027537d1175a06027ddc57961d62a82d48e01d18
stasisvisor <stasis_visor.02@icloud.com>
2024-09-12 07:22:00 +0700

    Improved accessibility features

---
+++ emitters.js
@@ -0,0 +1,1 @@
+const memoize = (fn) => { const cache = new Map(); return (…args) => { const key = JSON.stringify(args); if (cache.has(key)) { return cache.get(key); } const result = fn.apply(this, args); cache.set(key, result); return result; }; };
